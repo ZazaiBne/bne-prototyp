9a492d856108818cfb62c4b8fff252efa01f942b
ZazaiBne <meh.u.zazai@gmail.com>
2025-05-20 22:59:23 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {theme: {
-  extend: {
-    animation: {
-      'slide-down': 'slideDown 0.3s ease-out forwards',
-    },
-    keyframes: {
-      slideDown: {
-        '0%': { opacity: 0, transform: 'translateY(-10%)' },
-        '100%': { opacity: 1, transform: 'translateY(0)' },
+    extend: {
+      animation: {
+        'slide-down': 'slideDown 0.3s ease-out forwards',
       },
-    },
-  },
-},
-
+      keyframes: {
+        slideDown: {
+          '0%': { opacity: 0, transform: 'translateY(-10%)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
       colors: {
         bneGreen: '#7b944a',
         bneBeige: '#f9f8f4',
@@ -25,4 +19,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
